89a18efb181f4551e1a844e9067550cbebbce7ea
Renier Morales <renierm@us.ibm.com>
2016-08-22 15:55:46 -0400

    Add helper: Getter for NADC VIPs (by name)

---
+++ helpers/network/network.go
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package network
+
+import (
+	"fmt"
+
+	"github.ibm.com/riethm/gopherlayer.git/session"
+	"github.ibm.com/riethm/gopherlayer.git/services"
+	"github.ibm.com/riethm/gopherlayer.git/datatypes"
+	"github.ibm.com/riethm/gopherlayer.git/filter"
+)
+
+// GetNadcLbVip Get a virtual ip address by name attached to a load balancer
+// appliance like the Netscaler VPX. In the case of some load balancer appliances
+// looking up the virtual ip address by name is necessary since they don't get
+// assigned an id.
+func GetNadcLbVipByName(sess *session.Session, nadcId int, vipName string, mask ...string) (*datatypes.Network_LoadBalancer_VirtualIpAddress, error) {
+	service := services.GetNetworkApplicationDeliveryControllerService(sess)
+
+	service = service.
+		Id(nadcId).
+		Filter(filter.Path("name").Eq(vipName).Build())
+
+	if (len(mask) > 0) {
+		service = service.Mask(mask[0])
+	}
+
+	vips, err := service.GetLoadBalancers()
+	if err != nil {
+		return nil, fmt.Errorf("Error getting NADC load balancers: %s", err)
+	}
+
+	if len(vips) == 0 {
+		return nil, fmt.Errorf("Could not find any VIPs for NADC %d matching name %s", nadcId, vipName)
+	}
+
+	return &vips[0], nil
+}
